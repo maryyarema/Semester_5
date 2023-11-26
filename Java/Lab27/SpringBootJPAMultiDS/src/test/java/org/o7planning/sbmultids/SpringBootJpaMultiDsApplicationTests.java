dd436a122c1e548a735a9eb23240ad7ef4e96c36
maryyarema <yarema.mariia@chnu.edu.ua>
2023-11-26 12:16:38 +0200

    Add Labs

---
+++ Java/Lab27/SpringBootJPAMultiDS/src/test/java/org/o7planning/sbmultids/SpringBootJpaMultiDsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbmultids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaMultiDsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
